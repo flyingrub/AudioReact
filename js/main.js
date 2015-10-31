d6e04e053b24bb47a750b03ee6d6ebe026867e11
flyingrub <flyinggrub@gmail.com>
2015-10-31 15:59:25 +0100

    initial cleanup

--- js/main.js
+++ js/main.js
@@ -7,8 +7,7 @@ if (isMob()) {
     var player = document.getElementById('player')
     audio.init(player);
     visualInit()
-    scdlInit();
-    parseUrl();
+    audio.load("http://onde.xyz/pbb");
 }
 
 function canvasKeyfilter(event) {
@@ -22,18 +21,6 @@ function canvasKeyfilter(event) {
             case "f":
                 toggleFullScreen();
                 break;
-            case "ArrowDown":
-
-                break;
-            case "ArrowUp":
-
-                break;
-            case "ArrowLeft":
-                playPreviousFromTracklist();
-                break;
-            case "ArrowRight":
-                playNextFromTracklist();
-                break;
         }
     } else if (event.keyCode) { // support old browser like chromium.
         event.preventDefault();
@@ -43,18 +30,6 @@ function canvasKeyfilter(event) {
                 break;
             case 102:
                 toggleFullScreen();
-                break;
-            case 40:
-
-                break;
-            case 38:
-
-                break;
-            case 37:
-                playPreviousFromTracklist();
-                break;
-            case 39:
-                playNextFromTracklist();
                 break;
         }
     }
@@ -72,42 +47,6 @@ function isMob() {
         return true;
     } else {
         return false;
-    }
-}
-
-function parseUrl() {
-    url = window.location.pathname.split("/");
-    console.log(url)
-    switch (url[1]) {
-        case "users":
-            nav.show();
-            nav.searchMode();
-            getUser(url[2], res.user);
-            switch (url[3]) {
-                case "tracks":
-                    break;
-                case "playlists":
-                    document.getElementById('get-fav').className = "";
-                    document.getElementById("get-tracks").className = "";
-                    document.getElementById("get-playlist").className = "header-selected";
-                    getUserPlaylists(url[2], ser.displayPlaylists);
-                    break;
-            }
-            break;
-        case "track":
-            playFromId(url[2]);
-            break;
-        case "search":
-            nav.show();
-            nav.searchMode();
-            getTracks(search, res.search);
-            switch (url[2]) {
-                case "users":
-                    document.getElementById('get-tracks').className = "";
-                    document.getElementById("get-artists").className = "header-selected";
-                    getUsers(document.getElementById("search").value, ser.displayUsers);
-                    break;
-            }
     }
 }
 
